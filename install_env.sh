4015389cd3acc08587d9fce01de473d815ebd5cd
irsyad_ubuntu <irsyadadam@g.ucla.edu>
2025-03-09 22:32:55 -0700

    install pyg

--- install_env.sh
+++ install_env.sh
@@ -4,7 +4,6 @@ conda install -y pip
 
 #stl
 conda install -y pandas
-conda install -y numpy
 conda install -y matplotlib
 conda install -y seaborn
 conda install -y tqdm
@@ -13,16 +12,15 @@ conda install -y jupyter
 
 #data processing
 conda install -y scikit-learn
-conda install -y networkx
 conda install -y scipy
 
 #kernel
 python -m ipykernel install --user --name="pyg_CUDA"
 
-#ai stack (torch = 2.2, cuda = 11.8, pyg = 2.5.0) --> compatible with cuda 12.2
 conda install pytorch==2.2.1 torchvision=0.17.1 torchaudio=2.2.1 pytorch-cuda=11.8 -c pytorch -c nvidia
 
-#graph stack, pyg buggy with conda
 pip install torch_geometric
-pip install pyg_lib torch_scatter torch_sparse torch_cluster torch_spline_conv -f https://data.pyg.org/whl/torch-2.2.0+cu118.html
-pip install missingno
+pip install pyg_lib torch_scatter torch_sparse torch_cluster torch_spline_conv -f https://data.pyg.org/whl/torch-2.2.1+cu118.html
+
+pip install missingno networkx
+
