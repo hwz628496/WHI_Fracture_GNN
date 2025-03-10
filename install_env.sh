fa473a6ccf7f19c907bd81b2a3a9c6927e60357c
Irsyad Adam <irsyadadam@gpub004.delta.ncsa.illinois.edu>
2025-03-10 04:45:07 -0500

    no glibc, downgrade

--- install_env.sh
+++ install_env.sh
@@ -1,5 +1,5 @@
 
-#python = 3.11
+#python = 3.10, NO GLIBC_2.29
 conda install -y pip
 
 #stl
@@ -17,10 +17,20 @@ conda install -y scipy
 #kernel
 python -m ipykernel install --user --name="pyg_CUDA"
 
-conda install pytorch==2.2.1 torchvision=0.17.1 torchaudio=2.2.1 pytorch-cuda=11.8 -c pytorch -c nvidia
+conda install pytorch==2.1.0 torchvision==0.16.0 torchaudio==2.1.0 pytorch-cuda=12.1 -c pytorch -c nvidia -y
 
 pip install torch_geometric
-pip install pyg_lib torch_scatter torch_sparse torch_cluster torch_spline_conv -f https://data.pyg.org/whl/torch-2.2.1+cu118.html
 
-pip install missingno networkx
+# Install specific PyG versions
+pip install \
+    pyg_lib==0.3.1+pt21cu121 \
+    torch_scatter==2.1.2+pt21cu121 \
+    torch_sparse==0.6.18+pt21cu121 \
+    torch_cluster==1.6.3+pt21cu121 \
+    torch_spline_conv==1.2.2+pt21cu121 \
+    -f https://data.pyg.org/whl/torch-2.1.0+cu121.html
+
+
+pip install missingno networkx "numpy<2.0"
+
 
